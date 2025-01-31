dbc313cc42f5443dfa83ff25bdac5fce19397cca
Mohammedpashaf <145536112+Mohammedpashaf@users.noreply.github.com>
2025-01-31 16:09:35 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import './App.css';
 import './output.css';
 import './Custom.css';
+import StickyNavbar from './components/common/StickyNavbar.jsx';
+import Router from './Router.jsx';
 
- 
-import StickyNavbar from './pages/first page/StickyNavbar';
-import Carousel from './pages/first page/Carousel';
-import AboutUs from './pages/first page/AboutUs';
-import WhatWeDo from './pages/first page/WhatWeDo';
-import OurServers from './pages/first page/OurServers';
-import Contact from './pages/first page/Contact';
-import JionOur from './pages/first page/JoinOur';
-import Footer from './pages/first page/Footer';
-import Adoption from './pages/first page/Adoption';
 
 function App() {
   return (
     <div className="App">
       <StickyNavbar></StickyNavbar>
-      <Carousel></Carousel>
-      <AboutUs></AboutUs> 
-      <WhatWeDo></WhatWeDo>
-       <OurServers></OurServers>
-      <Adoption></Adoption>
-      <Contact></Contact>
-      <JionOur></JionOur>
-      <Footer></Footer>
+      <Router></Router>
     </div>
   );
 }
